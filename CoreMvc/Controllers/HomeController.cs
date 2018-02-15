c1388b17121ca2c463a44785c626fb8d21df6074
Ilya <tefc@yandex.ru>
2018-02-15 19:08:16 +0300

    Add project files.

---
+++ CoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using CoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+    }
+}
